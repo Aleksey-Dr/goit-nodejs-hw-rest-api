294365efc143829192e54b5d2665a04cc24dcd77
Aleksey-Dr <aprel181818@gmail.com>
2023-07-23 11:22:09 +0300

    Carry over Joi and callback functions
    1. Created a "schemas" folder and transfer Joi to inside her.
    2. Created a "controllers" folder and transfer callback functions to inside her.

---
+++ controllers/controllers.js
@@ -0,0 +1,77 @@
+const contacts = require("../models/contacts");
+const { HttpError } = require("../helpers");
+const addSchema = require("../schemas");
+
+const getAllContacts = async (req, res, next) => {
+  try {
+    res.json(await contacts.listContacts());
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addNewContact = async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, `missing ${error.message} field`);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({
+      message: "contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const changeContact = async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "missing fields");
+    }
+    const { contactId } = req.params;
+    const result = await contacts.updateContact(contactId, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getById,
+  addNewContact,
+  deleteContact,
+  changeContact,
+};
